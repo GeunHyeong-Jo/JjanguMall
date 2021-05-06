f783f2778da8cd921d4c9ad5e043f5f96dd88681
jgh0316 <jgh03@host.docker.internal>
2021-05-06 14:28:57 +0900

    add

--- JjanguMall/src/main/java/com/saltlux/jjangumall/controller/com/ComCartController.java
+++ JjanguMall/src/main/java/com/saltlux/jjangumall/controller/com/ComCartController.java
@@ -1,8 +1,9 @@
 package com.saltlux.jjangumall.controller.com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RequestMapping("com/cart")
 @Controller
 public class ComCartController {
 	
